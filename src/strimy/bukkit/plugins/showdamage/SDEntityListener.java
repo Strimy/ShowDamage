0ad4678bea40bf50210679529c696cad3a46dc67
Strimy <77113@supinfo.com>
2011-05-09 09:31:38 +0200

    Fix display of the same information multiple times

--- src/strimy/bukkit/plugins/showdamage/SDEntityListener.java
+++ src/strimy/bukkit/plugins/showdamage/SDEntityListener.java
@@ -1,5 +1,7 @@
 package strimy.bukkit.plugins.showdamage;
 
+import java.util.HashMap;
+
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -10,6 +12,7 @@ import org.bukkit.event.entity.EntityListener;
 
 public class SDEntityListener extends EntityListener  
 {
+	HashMap<Player, LastDamage> lastDamages = new HashMap<Player, LastDamage>();
 	ShowDamage plugin;
 	
 	public SDEntityListener(ShowDamage plugin)
@@ -31,12 +34,30 @@ public class SDEntityListener extends EntityListener
 			if(damager instanceof Player && entityEvent.getEntity() instanceof LivingEntity)
 			{
 				Player player = (Player)damager;
-				
 				LivingEntity entity = (LivingEntity)entityEvent.getEntity(); 
+				LastDamage d;
+				if(lastDamages.containsKey(player))
+				{
+					d = lastDamages.get(player);
+					if(d.getDamaged() == entity && d.getHealtLeft() == entity.getHealth())
+					{
+						super.onEntityDamage(event);
+						return;
+					}
+					lastDamages.remove(player);
+				}
+					
+				d = new LastDamage();
+				
+				
+				d.setDamaged(entity);
+				d.setHealtLeft(entity.getHealth());
+				lastDamages.put(player, d);
+				
 				int newHealth = entity.getHealth() - entityEvent.getDamage();
 				player.sendMessage(ChatColor.BLUE+ "You damaged " + entity.getClass().getSimpleName() + " with " + 
 						entityEvent.getDamage() + " ( "+ 
-						((newHealth < 0) ? 0 : newHealth) +")");
+						((newHealth < 0) ? 0 : newHealth) +" remaining)");
 			}
 		}
 		super.onEntityDamage(event);
